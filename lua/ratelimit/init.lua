4193e71de5b6b472c0bd5be071953be62d4bfb2d
kyoh86 <me@kyoh86.dev>
2025-03-02 10:24:41 +0900

    feat: provide a "ratelimit" module as a bind of all sub-modules

---
+++ lua/ratelimit/init.lua
@@ -0,0 +1,6 @@
+return {
+  debounce_leading = require("ratelimit.debounce").leading,
+  debounce_trailing = require("ratelimit.debounce").trailing,
+  throttle_leading = require("ratelimit.throttle").leading,
+  throttle_trailing = require("ratelimit.throttle").trailing,
+}
