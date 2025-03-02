ec622479fca609109f0f302a153e6cdbf4bd6413
kyoh86 <me@kyoh86.dev>
2025-03-02 10:21:49 +0900

    chore: validate functions with "function" instead of "f"

--- lua/ratelimit/args.lua
+++ lua/ratelimit/args.lua
@@ -3,7 +3,7 @@ local M = {}
 ---Validates args for `throttle()` and  `debounce()`.
 function M.validate(fn, ms)
   vim.validate({
-    fn = { fn, "f" },
+    fn = { fn, "function" },
     ms = {
       ms,
       function(v)
