f1e1fd9636f630d1e95345f36efd1bdf6016a141
kyoh86 <me@kyoh86.dev>
2025-04-09 19:46:11 +0900

    use vim.validate form 1 instead of 2

--- lua/ratelimit/args.lua
+++ lua/ratelimit/args.lua
@@ -2,16 +2,10 @@ local M = {}
 
 ---Validates args for `throttle()` and  `debounce()`.
 function M.validate(fn, ms)
-  vim.validate({
-    fn = { fn, "function" },
-    ms = {
-      ms,
-      function(v)
-        return type(v) == "number" and v > 0
-      end,
-      "number > 0",
-    },
-  })
+  vim.validate("fn", fn, "function")
+  vim.validate("ms", ms, function(v)
+    return type(v) == "number" and v > 0
+  end, "number > 0")
 end
 
 return M
